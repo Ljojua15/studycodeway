c7f9c7728aa844795b2e27246e8a5013817afec2
ljojua <jojualasha15@gmail.com>
2025-06-21 17:24:02 +0400

    feat:/ add pages

---
+++ src/app/pages/documentation/documentation.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'study-code-way-documentation',
+  imports: [],
+  templateUrl: './documentation.component.html',
+  styleUrl: './documentation.component.scss'
+})
+export class DocumentationComponent {
+
+}
---
+++ src/app/pages/flex-box/flex-box.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'study-code-way-flex-box',
+  imports: [],
+  templateUrl: './flex-box.component.html',
+  styleUrl: './flex-box.component.scss'
+})
+export class FlexBoxComponent {
+
+}
