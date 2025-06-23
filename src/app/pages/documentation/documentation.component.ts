42b7ec48af3f72134e8c0543d085b8d0ebe723e7
ljojua <ljojua@crocobet.com>
2025-06-23 10:45:40 +0400

    feat: start adding docs

--- src/app/pages/documentation/documentation.component.ts
+++ src/app/pages/documentation/documentation.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import {TechnologiesComponent} from '../../components/technologies/technologies.component';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'study-code-way-documentation',
-  imports: [],
+  imports: [
+    TechnologiesComponent,CommonModule
+  ],
   templateUrl: './documentation.component.html',
   styleUrl: './documentation.component.scss'
 })
